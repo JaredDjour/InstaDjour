99101a750ff38c41a088208d7a08e7fb134b36d1
JDjour <Jared.Djourabchi@gmail.com>
2019-05-29 15:07:14 -0400

    adds users reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,12 @@
+import merge from 'lodash/merge';
+import {RECEIVE_CURRENT_USER} from "../actions/session_actions"
+
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_CURRENT_USER:
+    return merge({}, state, {[action.currentUser.id]: currentUser.id})
+    default:
+    return state;
+  }
+}
