cd143ce3588ddd98bf13234e53506b8dc4e22ec9
JDjour <Jared.Djourabchi@gmail.com>
2019-05-29 22:48:27 -0400

    finishes session_form but doesn't render

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
 
 const App = () => (
+
   <div>
-      <h1> Welcome to InstaDjour, how can I help you </h1>
-      <h2> If you're seeing this, I've gots work to do </h2>
+      <header>
+      <Switch>
+      <Route path="/login" component={LoginFormContainer} />
+      <Route path="/signup" component={SignupFormContainer} />
+      </Switch>
+      </header>
+
   </div>
 );
 
