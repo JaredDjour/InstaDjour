6a3dfe7ca3a018d456440a622ecd9f7900aa9ca4
JDjour <Jared.Djourabchi@gmail.com>
2019-06-27 14:19:40 -0400

    adds condition for logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,29 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
-const configureStore = (preloadedState ={}) => {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger)
-    // applyMiddleware(thunk)
-  );
-};
+const middleWares = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger')
+  middleWares.push(logger);
+}
+export default (preloadedState) => (
+  createStore(rootReducer, preloadedState, applyMiddleware(...middleWares))
+);
 
-export default configureStore;
+
+// import { createStore, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
+// import rootReducer from '../reducers/root_reducer';
+
+// const configureStore = (preloadedState ={}) => {
+//   return createStore(
+//     rootReducer,
+//     preloadedState,
+//     applyMiddleware(thunk, logger)
+//     // applyMiddleware(thunk)
+//   );
+// };
+
+// export default configureStore;
