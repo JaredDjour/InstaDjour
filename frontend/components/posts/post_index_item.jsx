9e718c0eb4c9bce7fb32a1b607d86f763967d736
JDjour <Jared.Djourabchi@gmail.com>
2019-06-28 16:20:05 -0400

    improves button functionality

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -5,8 +5,10 @@ import {connect} from 'react-redux';
 import CommentIndexContainer from "../comments/comment_index_container";
 import {fetchAllComments} from "../../actions/comment_actions"; 
 const msp = (state, ownProps) => {
+    debugger
     return {
         username: state.entities.posts[ownProps.post.id].username,
+        currentUser: state.entities.users[state.session.id].username 
         // state.entities.posts[Ow]
         
     };
@@ -44,12 +46,20 @@ class PostIndexItem extends React.Component {
  
     render(){
         // const comments = this.props.posts.comments.reverse().map((comment) => <CommentContainer key={comment.id} comment={comment} deleteComment={this.props.deleteComment} />);
+        const deletePost = (this.props.username === this.props.currentUser) ?
+               (<li className="post-auth-options-delete" onClick={this.handleDelete}>Delete Post</li>) :
+               null;
+
         return (
             <div className="individual-post">
                 <div className="post-auth-container">
                     <div className="post-auth-image"></div>
                     <h2 className="post-auth" >{this.props.username}</h2>
                     <div className="post-auth-options"></div>
+                    <ul className="post-auth-options-list">
+                            {deletePost}
+                        <li><a href="/posts/${this.state.id">Show Post</a></li> 
+                    </ul>
                 </div>
                 <div className="post-photo-container">
                     <img className="photos" src={this.state.photoUrl}></img> 
