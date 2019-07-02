249eeefa3bb0eaf0e3ac8eaa0d7a9c27a42d5d0d
JDjour <Jared.Djourabchi@gmail.com>
2019-07-02 11:49:16 -0400

    completes comments

--- frontend/components/comments/comment_index_item.jsx
+++ frontend/components/comments/comment_index_item.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const msp = (state, ownProps) => {
-    debugger
     return {
         // username: state.entities.users[state.session.id].username,
         // username: state.entities.posts[ownProps.post.id].username,
